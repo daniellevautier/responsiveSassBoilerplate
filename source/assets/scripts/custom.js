1f068742d1e5c588f538cffa41b1f07f89f86319
unknown <DanielleVautier@ZoostormI7-Win7.cubeworks.local>
2015-03-30 12:05:12 +0100

    function to prevent orpahns

--- source/assets/scripts/custom.js
+++ source/assets/scripts/custom.js
@@ -1,1 +1,10 @@
-﻿
+﻿var preventOrphans = function() {
+	$("p").each(function() {
+	  var wordArray = $(this).text().split(" ");
+	  if (wordArray.length > 1) {
+	    wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
+	    wordArray.pop();
+	    $(this).html(wordArray.join(" "));
+	  }
+	});
+}
