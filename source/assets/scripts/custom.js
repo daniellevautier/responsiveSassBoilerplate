23b52fa826bb7e2aa9a7d7ee64517ebc8ce96bc6
Danielle Vautier <danielle.vauiter@yahoo.co.uk>
2015-03-30 22:03:36 +0100

    update to orphan function

--- source/assets/scripts/custom.js
+++ source/assets/scripts/custom.js
@@ -1,6 +1,7 @@
 ﻿var preventOrphans = function(element) {
 	//Would suggest this is used for title text mainly
-	$('p').each(function(){
+
+	$(element).each(function(){
 	    var string = $(this).html();
 	    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
 	    $(this).html(string);
